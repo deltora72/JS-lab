19e155ae421aec0bb52cd41ae65165d798f1ec93
deltora72 <mohammad.j.dim@gmail.com>
2019-03-05 18:56:53 +0200

    Initial commit

---
+++ scripts/positions.js
@@ -0,0 +1,511 @@
+let positions = {
+  0: {
+    pos: "000",
+    det: [
+      { text: "3 1xBAR symbols on any line", prize: 10, line: "center" }
+    ]
+  },
+  1: {
+    pos: "111",
+    det: [
+      { text: "3 3xBAR symbols on any line", prize: 50, line: "top" },
+      { text: "3 1xBAR symbols on any line", prize: 10, line: "bottom" }
+    ]
+  },
+  2: {
+    pos: "222",
+    det: [
+      { text: "3 3xBAR symbols on any line", prize: 50, line: "center" }
+    ]
+  },
+  3: {
+    pos: "333",
+    det: [
+      { text: "3 CHERRY symbols on top line", prize: 2000, line: "top" },
+      { text: "3 3xBAR symbols on any line", prize: 50, line: "bottom" }
+    ]
+  },
+  4: {
+    pos: "444",
+    det: [
+      { text: "3 CHERRY symbols on center line", prize: 1000, line: "center" }
+    ]
+  },
+  5: {
+    pos: "555",
+    det: [
+      { text: "3 CHERRY symbols on bottom line", prize: 4000, line: "bottom" },
+      { text: "3 '7' symbols on any line", prize: 150, line: "top" }
+    ]
+  },
+  6: {
+    pos: "666",
+    det: [
+      { text: "3 '7' symbols on any line", prize: 150, line: "center" }
+    ]
+  },
+  7: {
+    pos: "777",
+    det: [
+      { text: "3 2xBAR symbols on any line", prize: 20, line: "top" },
+      { text: "3 '7' symbols on any line", prize: 150, line: "bottom" }
+    ]
+  },
+  8: {
+    pos: "888",
+    det: [
+      { text: "3 2xBAR symbols on any line", prize: 20, line: "center" }
+    ]
+  },
+  9: {
+    pos: "999",
+    det: [
+      { text: "3 1xBAR symbols on any line", prize: 10, line: "top" },
+      { text: "3 2xBAR symbols on any line", prize: 20, line: "bottom" }
+    ]
+  },
+  10: {
+    pos: "335",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "top" }
+    ]
+  },
+  11: {
+    pos: "353",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "top" }
+    ]
+  },
+  12: {
+    pos: "533",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "top" }
+    ]
+  },
+  13: {
+    pos: "553",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "top" }
+    ]
+  },
+  14: {
+    pos: "535",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "top" }
+    ]
+  },
+  15: {
+    pos: "355",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "top" }
+    ]
+  },
+  16: {
+    pos: "446",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "center" }
+    ]
+  },
+  17: {
+    pos: "464",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "center" }
+    ]
+  },
+  18: {
+    pos: "644",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "center" }
+    ]
+  },
+  19: {
+    pos: "664",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "center" }
+    ]
+  },
+  20: {
+    pos: "646",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "center" }
+    ]
+  },
+  21: {
+    pos: "466",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "center" }
+    ]
+  },
+  22: {
+    pos: "557",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "bottom" }
+    ]
+  },
+  23: {
+    pos: "575",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "bottom" }
+    ]
+  },
+  24: {
+    pos: "755",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "bottom" }
+    ]
+  },
+  25: {
+    pos: "775",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "bottom" }
+    ]
+  },
+  26: {
+    pos: "757",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "bottom" }
+    ]
+  },
+  27: {
+    pos: "577",
+    det: [
+      { text: "Any combination of CHERRY and '7' on any line", prize: 75, line: "bottom" }
+    ]
+  },
+  28: {
+    pos: "997",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" }
+    ]
+  },
+  29: {
+    pos: "979",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" }
+    ]
+  },
+  30: {
+    pos: "799",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" }
+    ]
+  },
+  31: {
+    pos: "779",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" }
+    ]
+  },
+  32: {
+    pos: "797",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" }
+    ]
+  },
+  33: {
+    pos: "977",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" }
+    ]
+  },
+  34: {
+    pos: "991",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" },
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  35: {
+    pos: "919",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" },
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  36: {
+    pos: "199",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" },
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  37: {
+    pos: "119",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" },
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  38: {
+    pos: "191",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" },
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  39: {
+    pos: "911",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" },
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  40: {
+    pos: "771",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" }
+    ]
+  },
+  41: {
+    pos: "717",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" }
+    ]
+  },
+  42: {
+    pos: "177",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" }
+    ]
+  },
+  43: {
+    pos: "391",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  44: {
+    pos: "319",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  45: {
+    pos: "913",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  46: {
+    pos: "931",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  47: {
+    pos: "193",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  48: {
+    pos: "139",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  49: {
+    pos: "117",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" }
+    ]
+  },
+  50: {
+    pos: "171",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" }
+    ]
+  },
+  51: {
+    pos: "711",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "top" }
+    ]
+  },
+  51: {
+    pos: "008",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  52: {
+    pos: "080",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  53: {
+    pos: "800",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  54: {
+    pos: "880",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  55: {
+    pos: "808",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  56: {
+    pos: "088",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  57: {
+    pos: "002",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  59: {
+    pos: "020",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  60: {
+    pos: "200",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  61: {
+    pos: "220",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  62: {
+    pos: "202",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  63: {
+    pos: "022",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  64: {
+    pos: "882",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  65: {
+    pos: "828",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  66: {
+    pos: "288",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  67: {
+    pos: "228",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  68: {
+    pos: "282",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  69: {
+    pos: "822",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "center" }
+    ]
+  },
+  70: {
+    pos: "113",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  71: {
+    pos: "131",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  72: {
+    pos: "311",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  73: {
+    pos: "311",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  74: {
+    pos: "331",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  75: {
+    pos: "313",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  76: {
+    pos: "133",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  77: {
+    pos: "993",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  78: {
+    pos: "939",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  79: {
+    pos: "399",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  80: {
+    pos: "339",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  81: {
+    pos: "393",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  },
+  82: {
+    pos: "933",
+    det: [
+      { text: "Combination of any 'BAR' symbols on any line", prize: 5, line: "bottom" }
+    ]
+  }
+}
---
+++ scripts/scripts.js
@@ -0,0 +1,336 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    reelPos: 0,
+    reelHeight: document.querySelectorAll('.reel')[0].offsetHeight,
+    spinOneCount: 0,
+    spinTwoCount: 0,
+    spinThreeCount: 0,
+    spinTime: 10,
+    pics: [
+      {id: "pic one", src: "pics/3xBAR.png"},
+      {id: "pic two", src: "pics/BAR.png"},
+      {id: "pic three", src: "pics/2xBAR.png"},
+      {id: "pic four", src: "pics/7.png"},
+      {id: "pic five", src: "pics/cherry.png"}
+    ],
+    balance: 5000,
+    rIsSpinning: false,
+    fIsSpinning: false,
+    gameMode: 'random',
+    fmode: 'fixed',
+    rmode: 'random',
+    randVis: true,
+    lineTop: false,
+    lineCenter: false,
+    lineBottom: false,
+    curBalance: 5,
+    coins: '',
+    balanceAlert: false,
+    fixVis: false,
+    symbolList: ["1xBAR", "2xBAR", "3xBAR", "7", "CHERRY"],
+    symbolLanding: ["Top", "Center", "Bottom"],
+    cS1: '',
+    cL1: '',
+    cS2: '',
+    cL2: '',
+    cS3: '',
+    cL3: '',
+    payoutsHeaders: [
+      { text: 'Combination', align: 'left', sortable: false, value: 'calories' },
+      { text: 'Prize', align: 'right', sortable: false, value: 'calories' }
+    ],
+    payouts: [
+      { combination: "3 CHERRY symbols on top line", prize: 2000 },
+      { combination: "3 CHERRY symbols on center line", prize: 1000 },
+      { combination: "3 CHERRY symbols on bottom line", prize: 4000 },
+      { combination: "3 '7' symbols on any line", prize: 150 },
+      { combination: "Any combination of CHERRY and '7' on any line", prize: 75 },
+      { combination: "3 3xBAR symbols on any line", prize: 50 },
+      { combination: "3 2xBAR symbols on any line", prize: 20 },
+      { combination: "3 1xBAR symbols on any line", prize: 10 },
+      { combination: "Combination of any 'BAR' symbols on any line", prize: 5 }
+    ],
+    curPosition: [0,0,0],
+    playerWins: 0,
+    newWin: 0
+  },
+  // watch: {
+  //   spinOneCount: function (val) {
+  //     console.log(val);
+  //   }
+  // },
+  methods: {
+    fixedSpin: function () {
+      this.spinOneCount = this.spinTwoCount = this.spinThreeCount = 0;
+      for (let tr of document.querySelectorAll('.payouts table tbody tr')) {
+        tr.style.color = "#000000de";
+        tr.style.backgroundColor = "#fff";
+      }
+      this.fIsSpinning = true;
+      this.lineTop = false;
+      this.lineCenter = false;
+      this.lineBottom = false;
+      // reel one fixed
+      if (this.cS1 === "CHERRY" && this.cL1 === "Top" || this.cS1 === "3xBAR" && this.cL1 === "Bottom") {
+        this.resetReels(); this.spinOne(203);
+      } else if (this.cS1 === "CHERRY" && this.cL1 === "Center") {
+        this.resetReels(); this.spinOne(204);
+      } else if (this.cS1 === "CHERRY" && this.cL1 === "Bottom" || this.cS1 === "7" && this.cL1 === "Top") {
+        this.resetReels(); this.spinOne(205);
+      } else if (this.cS1 === "7" && this.cL1 === "Center") {
+        this.resetReels(); this.spinOne(206);
+      } else if (this.cS1 === "7" && this.cL1 === "Bottom" || this.cS1 === "2xBAR" && this.cL1 === "Top") {
+        this.resetReels(); this.spinOne(207);
+      } else if (this.cS1 === "2xBAR" && this.cL1 === "Center") {
+        this.resetReels(); this.spinOne(208);
+      } else if (this.cS1 === "2xBAR" && this.cL1 === "Bottom" || this.cS1 === "1xBAR" && this.cL1 === "Top") {
+        this.resetReels(); this.spinOne(209);
+      } else if (this.cS1 === "1xBAR" && this.cL1 === "Center") {
+        this.resetReels(); this.spinOne(200);
+      } else if (this.cS1 === "1xBAR" && this.cL1 === "Bottom" || this.cS1 === "3xBAR" && this.cL1 === "Top") {
+        this.resetReels(); this.spinOne(201);
+      } else if (this.cS1 === "3xBAR" && this.cL1 === "Center") {
+        this.resetReels(); this.spinOne(202);
+      }
+
+      // reel two fixed
+      if (this.cS2 === "CHERRY" && this.cL2 === "Top" || this.cS2 === "3xBAR" && this.cL2 === "Bottom") {
+        this.resetReels(); this.spinTwo(203);
+      } else if (this.cS2 === "CHERRY" && this.cL2 === "Center") {
+        this.resetReels(); this.spinTwo(204);
+      } else if (this.cS2 === "CHERRY" && this.cL2 === "Bottom" || this.cS2 === "7" && this.cL2 === "Top") {
+        this.resetReels(); this.spinTwo(205);
+      } else if (this.cS2 === "7" && this.cL2 === "Center") {
+        this.resetReels(); this.spinTwo(206);
+      } else if (this.cS2 === "7" && this.cL2 === "Bottom" || this.cS2 === "2xBAR" && this.cL2 === "Top") {
+        this.resetReels(); this.spinTwo(207);
+      } else if (this.cS2 === "2xBAR" && this.cL2 === "Center") {
+        this.resetReels(); this.spinTwo(208);
+      } else if (this.cS2 === "2xBAR" && this.cL2 === "Bottom" || this.cS2 === "1xBAR" && this.cL2 === "Top") {
+        this.resetReels(); this.spinTwo(209);
+      } else if (this.cS2 === "1xBAR" && this.cL2 === "Center") {
+        this.resetReels(); this.spinTwo(200);
+      } else if (this.cS2 === "1xBAR" && this.cL2 === "Bottom" || this.cS2 === "3xBAR" && this.cL2 === "Top") {
+        this.resetReels(); this.spinTwo(201);
+      } else if (this.cS2 === "3xBAR" && this.cL2 === "Center") {
+        this.resetReels(); this.spinTwo(202);
+      }
+
+      // reel two fixed
+      if (this.cS3 === "CHERRY" && this.cL3 === "Top" || this.cS3 === "3xBAR" && this.cL3 === "Bottom") {
+        this.resetReels(); this.spinThree(203);
+      } else if (this.cS3 === "CHERRY" && this.cL3 === "Center") {
+        this.resetReels(); this.spinThree(204);
+      } else if (this.cS3 === "CHERRY" && this.cL3 === "Bottom" || this.cS3 === "7" && this.cL3 === "Top") {
+        this.resetReels(); this.spinThree(205);
+      } else if (this.cS3 === "7" && this.cL3 === "Center") {
+        this.resetReels(); this.spinThree(206);
+      } else if (this.cS3 === "7" && this.cL3 === "Bottom" || this.cS3 === "2xBAR" && this.cL3 === "Top") {
+        this.resetReels(); this.spinThree(207);
+      } else if (this.cS3 === "2xBAR" && this.cL3 === "Center") {
+        this.resetReels(); this.spinThree(208);
+      } else if (this.cS3 === "2xBAR" && this.cL3 === "Bottom" || this.cS3 === "1xBAR" && this.cL3 === "Top") {
+        this.resetReels(); this.spinThree(209);
+      } else if (this.cS3 === "1xBAR" && this.cL3 === "Center") {
+        this.resetReels(); this.spinThree(200);
+      } else if (this.cS3 === "1xBAR" && this.cL3 === "Bottom" || this.cS3 === "3xBAR" && this.cL3 === "Top") {
+        this.resetReels(); this.spinThree(201);
+      } else if (this.cS3 === "3xBAR" && this.cL3 === "Center") {
+        this.resetReels(); this.spinThree(202);
+      }
+    },
+    randomSpin: function () {
+      if (this.curBalance > 0) {
+        this.curBalance--;
+        this.spinOneCount = this.spinTwoCount = this.spinThreeCount = 0;
+        self.rIsSpinning = true;
+        this.spinOne(Math.floor(Math.random()*100 + 200));
+        this.spinTwo(Math.floor(Math.random()*100 + 250));
+        this.spinThree(Math.floor(Math.random()*100 + 300));
+        for (let tr of document.querySelectorAll('.payouts table tbody tr')) {
+          tr.style.color = "#000000de";
+          tr.style.backgroundColor = "#fff";
+        }
+        this.lineTop = false; this.lineCenter = false; this.lineBottom = false;
+      } else {
+        this.balanceAlert = true;
+      }
+    },
+    spinOne: function (spinsCount) {
+      let reelPics = document.querySelectorAll('#reel1 .pic');
+      // spinning function
+      const self = this;
+      let currentSpinsCount = 0;
+      let anim = setInterval(function () {
+        if (currentSpinsCount < spinsCount) {
+          // reeling function -- moves the reel one position down
+          for (let i of reelPics) {
+            if (i.offsetTop === self.reelHeight * 1.75) {
+              i.style.top = -self.reelHeight * .50 +'px';
+              i.style.zIndex = '-2';
+            } else {
+              i.style.zIndex = '1';
+              self.reelPos = i.offsetTop + self.reelHeight * .25;
+              i.style.top = self.reelPos +'px';
+            }
+          }
+          currentSpinsCount++;
+        } else {
+          self.spinOneCount = spinsCount;
+          clearInterval(anim);
+          currentSpinsCount = 0;
+          self.spinFinished();
+        }
+      }, this.spinTime); // Sets tiem intervals for reel repeats
+    },
+    spinTwo: function (spinsCount) {
+      let reelPics = document.querySelectorAll('#reel2 .pic');
+      // spinning function
+      this.rIsSpinning = true;
+      const self = this;
+      let currentSpinsCount = 0;
+      let anim = setInterval(function () {
+        if (currentSpinsCount < spinsCount) {
+          // reeling function -- moves the reel one position down
+          for (let i of reelPics) {
+            if (i.offsetTop === self.reelHeight * 1.75) {
+              i.style.top = -self.reelHeight * .50 +'px';
+              i.style.zIndex = '-2';
+            } else {
+              i.style.zIndex = '1';
+              self.reelPos = i.offsetTop + self.reelHeight * .25;
+              i.style.top = self.reelPos +'px';
+            }
+          }
+          currentSpinsCount++;
+        } else {
+          self.spinTwoCount = spinsCount;
+          clearInterval(anim);
+          currentSpinsCount = 0;
+          self.spinFinished();
+        }
+      }, this.spinTime); // Sets tiem intervals for reel repeats
+    },
+    spinThree: function (spinsCount) {
+      let reelPics = document.querySelectorAll('#reel3 .pic');
+      // spinning function
+      this.rIsSpinning = true;
+      const self = this;
+      let currentSpinsCount = 0;
+      let anim = setInterval(function () {
+        if (currentSpinsCount < spinsCount) {
+          // reeling function -- moves the reel one position down
+          for (let i of reelPics) {
+            if (i.offsetTop === self.reelHeight * 1.75) {
+              i.style.top = -self.reelHeight * .50 +'px';
+              i.style.zIndex = '-2';
+            } else {
+              i.style.zIndex = '1';
+              self.reelPos = i.offsetTop + self.reelHeight * .25;
+              i.style.top = self.reelPos +'px';
+            }
+          }
+          currentSpinsCount++;
+        } else {
+          self.spinThreeCount = spinsCount;
+          clearInterval(anim);
+          currentSpinsCount = 0;
+          self.spinFinished();
+        }
+      }, this.spinTime); // Sets tiem intervals for reel repeats
+    },
+    resetReels: function () {
+      document.getElementsByClassName('pic one')[0].style.top = "-25%";
+      document.getElementsByClassName('pic two')[0].style.top = "25%";
+      document.getElementsByClassName('pic three')[0].style.top = "75%";
+      document.getElementsByClassName('pic four')[0].style.top = "125%";
+      document.getElementsByClassName('pic five')[0].style.top = "175%";
+      document.getElementsByClassName('pic one')[1].style.top = "-25%";
+      document.getElementsByClassName('pic two')[1].style.top = "25%";
+      document.getElementsByClassName('pic three')[1].style.top = "75%";
+      document.getElementsByClassName('pic four')[1].style.top = "125%";
+      document.getElementsByClassName('pic five')[1].style.top = "175%";
+      document.getElementsByClassName('pic one')[2].style.top = "-25%";
+      document.getElementsByClassName('pic two')[2].style.top = "25%";
+      document.getElementsByClassName('pic three')[2].style.top = "75%";
+      document.getElementsByClassName('pic four')[2].style.top = "125%";
+      document.getElementsByClassName('pic five')[2].style.top = "175%";
+      this.curPosition = [0,0,0];
+      this.lineTop = false; this.lineCenter = false; this.lineBottom = false;
+    },
+    ModeSelector: function () {
+      if (this.gameMode === 'random') {
+        this.randVis = true;
+        this.fixVis = false;
+      } else {
+        this.randVis = false;
+        this.fixVis = true;
+      }
+    },
+    addBalance: function () {
+      if (this.coins > 0 && this.coins <= 5000) {
+        this.addCoinsError = false;
+        this.curBalance = Number(this.coins);
+      }
+    },
+    spinFinished: function () {
+      if (this.spinOneCount !== 0 && this.spinTwoCount !== 0 && this.spinThreeCount !== 0) {
+        let posOneDigit = this.spinOneCount % 10;
+        let posTwoDigit = this.spinTwoCount % 10;
+        let posThreeDigit = this.spinThreeCount % 10;
+
+        if (posOneDigit === 0) {
+          this.curPosition[0] = 0;
+        } else {
+          this.curPosition[0] = (this.curPosition[0] + posOneDigit) % 10;
+        }
+        if (posTwoDigit === 0) {
+          this.curPosition[1] = 0;
+        } else {
+          this.curPosition[1] = (this.curPosition[1] + posTwoDigit) % 10;
+        }
+        if (posThreeDigit === 0) {
+          this.curPosition[2] = 0;
+        } else {
+          this.curPosition[2] = (this.curPosition[2] + posThreeDigit) % 10;
+        }
+        this.rIsSpinning = false;
+        this.fIsSpinning = false;
+        this.payout();
+      }
+    },
+    payout: function () {
+      this.newWin = 0;
+      let payRow;
+      for (let i in positions) {
+        if (positions[i].pos === this.curPosition.join('')) {
+          for (let j of positions[i].det) {
+            this.newWin += j.prize;
+            for (let comb of this.payouts) {
+              if (comb.combination === j.text) {
+                console.log();
+                payRow = document.querySelectorAll('.payouts table tbody tr')[this.payouts.indexOf(comb)];
+                payRow.style.color = "#fff";
+                payRow.style.backgroundColor = "#f44336";
+                if (j.line === "top") {
+                  this.lineTop = true;
+                } else if (j.line === "center") {
+                  this.lineCenter = true;
+                } else {
+                  this.lineBottom = true;
+                }
+              }
+            }
+          }
+          this.playerWins += this.newWin;
+          this.curBalance += this.newWin;
+        }
+      }
+    }
+  }
+});
+
+});
